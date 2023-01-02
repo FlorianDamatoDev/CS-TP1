d78afff89588e933ad1a45e2371d20ae58d92e46
FlorianDamatoDev <floriandamato.dev@gmail.com>
2023-01-02 12:26:43 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿public enum JoursSemaine {
+    Lundi,
+    Mardi,
+    Mercredi,
+    Jeudi,
+    Vendredi,
+    Samedi,
+    Dimanche
+};
+
+public class Program {
+    public static void Main()
+    {
+        JoursSemaine today = JoursSemaine.Lundi;
+        Console.WriteLine("Nous sommes " + today);
+    }
+};
---
+++ cs.md
@@ -0,0 +1,13 @@
+# Cours CS
+
+
+## Components
+
+1. Dotnet (.NET)
+2. VS Code
+3. Extension C#
+
+## Terminal
+
+1. $ dotnet new console
+2. $ dotnet run
