eb6ab1f965fc7f82827bd636d23ed3b8916c755d
FlorianDamatoDev <floriandamato.dev@gmail.com>
2023-01-02 15:40:27 +0100

    exo 10 : liste couleurs

--- Program.cs
+++ Program.cs
@@ -129,18 +129,31 @@ public class Program {
 // EXO 9 //
 //-------//
 
-            int[] tabNumbers = new int[10];
+        int[] tabNumbers = new int[10];
 
-            Console.WriteLine("Veuillez entrer 10 nombres :");
-            for (int i = 0; i < 10; i++) {
-                Console.Write("Numéro {0} : ", i + 1);
-                tabNumbers[i] = int.Parse(Console.ReadLine());
-            }
+        Console.WriteLine("Veuillez entrer 10 nombres :");
+        for (int i = 0; i < 10; i++) {
+            Console.Write("Numéro {0} : ", i + 1);
+            tabNumbers[i] = int.Parse(Console.ReadLine());
+        }
 
-            Console.WriteLine("Voici les nombres que vous avez entrés :");
-            foreach (int tabNumber in tabNumbers) {
-                Console.WriteLine(tabNumber);
-            }
+        Console.WriteLine("Voici les nombres que vous avez entrés :");
+        foreach (int tabNumber in tabNumbers) {
+            Console.WriteLine(tabNumber);
+        }
+
+//--------//
+// EXO 10 //
+//--------//
+
+        string[] colors = new string[] { "rouge", "vert", "bleu", "jaune", "orange" };
+
+        int i = 0;
+        while (i < colors.Length)
+        {
+            Console.WriteLine(colors[i]);
+            i++;
+        }
 
     }
 };
